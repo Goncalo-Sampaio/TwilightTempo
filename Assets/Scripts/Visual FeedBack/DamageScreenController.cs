4afe5ee0cefaf0090c5000f0ff30fa138c753834
Eduardo Rocha <edu.sang.rocha@gmail.com>
2026-02-24 08:49:05 +0000

    Thats alot of damage

--- Assets/Scripts/Visual FeedBack/DamageScreenController.cs
+++ Assets/Scripts/Visual FeedBack/DamageScreenController.cs
@@ -6,7 +6,7 @@ public class DamageScreenController : MonoBehaviour
 {
     [SerializeField]private CinemachineImpulseSource _impulseSource;
     [SerializeField]private Material screenDamageMat;
-    [SerializeField] private float lerpSpeed = 1f;
+    [SerializeField] private float impactLerpSpeed = 6f;//a good value
     private Coroutine screenDamageRoutine;
 
     //Make this a static singleton so other objects can call the DamageEffect easier
@@ -33,13 +33,13 @@ public class DamageScreenController : MonoBehaviour
         float targetRadius = Remap(intensity, 0f, 1f, 0.4f, -0.15f);
         float currentRadius = 1; //No damage
         //This section might need to be added with higher speed or just instant. Right now it takes too long to kick in. Lagging behind the camera shake:
-        for(float t = 0;currentRadius != targetRadius ;t += Time.deltaTime * lerpSpeed)
+        for(float t = 0;currentRadius != targetRadius ;t += Time.deltaTime * impactLerpSpeed)
         {
             currentRadius = Mathf.Lerp(1, targetRadius, t);
             screenDamageMat.SetFloat("_Vignette_Radius", currentRadius);
             yield return null;
         }
-        for (float t = 0f; currentRadius < 1f; t += Time.deltaTime * lerpSpeed)
+        for (float t = 0f; currentRadius < 1f; t += Time.deltaTime )
         {
             currentRadius = Mathf.Lerp(targetRadius, 1, t);
             //currentRadius = Mathf.Lerp(targetRadius, 1f, Mathf.SmoothStep(0.0f, 1.0f, t));
