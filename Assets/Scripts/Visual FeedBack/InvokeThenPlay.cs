d0fa62da364308e6f1cacb1bdc80d6e071260159
Eduardo Rocha <edu.sang.rocha@gmail.com>
2026-03-16 21:12:14 +0000

    Build Scene + Fixed button

---
+++ Assets/Scripts/Visual FeedBack/InvokeThenPlay.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InvokeThenPlay : MonoBehaviour
+{
+    [SerializeField] AudioSource buttonSound;
+    
+    public void PlayWithDelay()
+    {
+        if (buttonSound != null)
+        {
+            buttonSound.Play();
+        }
+    }
+    public void PlayWithDelayAndChangeScene()
+    {
+        if (buttonSound != null)
+        {
+            buttonSound.Play();
+        }
+        StartCoroutine(ChangeToRot());
+    }
+    private IEnumerator ChangeToRot()
+    {
+        yield return null;
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(1);
+    }
+   
+}
